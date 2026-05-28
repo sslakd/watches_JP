4fc65203cd7221fbe3a5aa647c18499beea852df
Son Nguyen <sonnguyen@Sons-Mac-mini.local>
2026-05-28 09:19:18 +0700

    Add sitemap integration + robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://tokeimonogatari.com',
   outDir: './dist',
-  build: { format: 'file' }
+  build: { format: 'file' },
+  integrations: [sitemap()]
 });
